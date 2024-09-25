d60fd381702ac17e34575c7d58559f1d08f11786
KCui0327 <kenny.cui@mail.utoronto.ca>
2024-09-25 13:43:32 -0400

    Reproduced Example 4-7 and added email validation
    - checks if email is a valid email and if it is a 'utoronto' email
    - throws error messages if there's any errors

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # E444-F2024-PRA2: Kenny Cui
 
 > This repository is a clone of [https://github.com/miguelgrinberg/flasky](https://github.com/miguelgrinberg/flaskyhttps:/) with updates and changes made by myself.
+
+### Activity 1.4
+
+![](img/activity_1.4.png)
--- examples/example3_exercises.py
+++ examples/example3_exercises.py
@@ -1,30 +1,55 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-class NameForm(FlaskForm):
+class Form(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        valid = True
+        err_msg = ""
+        regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(regex, field.data):
+            valid = False
+            err_msg += 'Email is not a valid email address. '
+        
+        if 'utoronto' not in field.data:
+            valid = False
+            err_msg += 'Email must be a UofT email address.'
+
+        if not valid:
+            raise ValidationError(err_msg)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = Form()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        old_email=session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    
+    if form.name.data:
+        session['name'] = form.name.data
+    
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<user_name>')
 def user(user_name):
